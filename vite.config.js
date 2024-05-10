c52e96f009f05ec848c6cff0aab36f91c7454091
ShallyC20 <carlymar204@gmail.com>
2024-05-09 23:22:29 -0400

    Prin6

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://shallyc20.github.io/ejer/ "
+  base: "https://shallyc20.github.io/Ejer/ "
 })
 // https://github.com/ShallyC20/Ejer.git
